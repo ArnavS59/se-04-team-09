e5023c18ee0f577fa5ceb755af3b6db7dc79730c
Zhengkaluo <41323348+Zhengkaluo@users.noreply.github.com>
2021-03-17 22:50:32 +0100

    mysql settings update

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'test'
+app.config['MYSQL_PASSWORD'] = 'xalhna'
+app.config['MYSQL_DB'] = 'seteam09'
 
 mysql = MySQL()
 mysql.init_app(app)
